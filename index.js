5b697444901ab257b0617da597686a25c58739a6
Lellansin <lellansin@gmail.com>
2016-05-19 16:33:07 +0800

    Update data save method

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 
 const Redis = require('ioredis')
 const schedule = require('node-schedule')
+const moment = require('moment')
 
 class Monitor {
   constructor (config) {
     this.config = config
     this.name = config.name
-    this.pub = new Redis(config.redis)
+    this.redis = new Redis(config.redis)
     let time = config.time || '*/1 * * * *'
     schedule.scheduleJob(time, this.report.bind(this))
     this.cache = {}
@@ -24,24 +25,30 @@ class Monitor {
   }
 
   report () {
-    let sys = {
-      name: this.name,
-      pid: process.pid,
-      mem: process.memoryUsage(),
-      uptime: process.uptime(),
-      ver: process.versions.node,
-      time: new Date()
-    }
-    let data = {}
+    let pid = process.pid, now = moment(),
+      today = now.format('YYYY-MM-DD'),
+      minute = now.format('HH:mm')
+    let res = {
+      data: {},
+      sys: {
+        name: this.name,
+        pid: pid,
+        mem: process.memoryUsage(),
+        uptime: process.uptime(),
+        ver: process.versions.node,
+        time: new Date()
+    }}
+
     for (var key in this.cache) {
-      data[key] = this.cache[key]
+      res.data[key] = this.cache[key]
       delete this.cache[key]
     }
 
-    this.pub.publish(this.config.channel, JSON.stringify({
-      sys: sys,
-      data: data
-    }))
+    let str = JSON.stringify(res)
+
+    this.redis.publish(this.config.channel, `MSG:alive||${this.name}||${pid}`)
+    this.redis.setex(`${this.config.channel}-process|${this.name}|${pid}`, 61) // 61 秒过期
+    this.redis.hset(`${this.config.channel}|${today}|${this.name}|${pid}`, minute, str)
   }
 }
 
